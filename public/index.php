7b50217fc40e9a96b90f4ddbe037424ff8a341cb
Trey Piepmeier <trey@treypiepmeier.com>
2007-11-11 18:39:08 -0500

    Removed app/controller.php and moved its contents to public/index.php

--- public/index.php
+++ public/index.php
@@ -3,9 +3,27 @@
 require_once('../app/load.php');
 // Load old school procedural functions
 require_once('../app/functions.php');
-require_once('../app/controller.php');
 
 // $content = SmartyPants(Markdown(file_get_contents(CONTENT_PATH . '/' . $page_name . '.txt')));
 // display_haml(TEMPLATE_PATH . '/' . $template_name . '.haml', array($site, $content), HAML_TEMP_PATH);
+
+
+// [old controller.php]
+// TODO: somehow determine which template to use to render it (template_name)
+//       use section_name or fall back to index
+
+$template = $page;
+display_page($section, $subsection, $page, $template);
+
+	// Section
+
+		// Sub-section
+
+	// or Error
+
+	// ---
+	
+// Find Haml template needed
+	// (Find template type)
 
 ?>
