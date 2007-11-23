d77092bf3f9478df6eba6d19adb0024c78f11de9
Trey Piepmeier <trey@treypiepmeier.com>
2007-11-23 11:28:26 -0500

    Cleaning up what the debug panel shows and what template is used.  Now uses error template if the page isn't found but still shows what the section was supposed to be in the debug panel.

--- app/functions.php
+++ app/functions.php
@@ -1,9 +1,11 @@
 <?php
-function find_template($section)
+function find_template($page)
 {
+	if ($page['path'] = 'errors/404') $page['section'] = 'error';
+	
 	$php_exists = $haml_exists = false;
-	if (file_exists(TEMPLATE_PATH . '/' . $section . '.php'))  $php_exists  = true;
-	if (file_exists(TEMPLATE_PATH . '/' . $section . '.haml')) $haml_exists = true;
+	if (file_exists(TEMPLATE_PATH . '/' . $page['section'] . '.php'))  $php_exists  = true;
+	if (file_exists(TEMPLATE_PATH . '/' . $page['section'] . '.haml')) $haml_exists = true;
 
 	// Determine which template to use
 	switch (SITE_TEMPLATE_TYPE) {
@@ -11,11 +13,11 @@ function find_template($section)
 			if ($php_exists) {
 				// PHP template exists -- use it.
 				$template['type'] = 'php';
-				$template['name'] = $section;
+				$template['name'] = $page['section'];
 			} elseif ($haml_exists) {
 				// Wanted PHP, but it's not there and the Haml is -- use it instead.
 				$template['type'] = 'haml';
-				$template['name'] = $section;
+				$template['name'] = $page['section'];
 			} else {
 				// No template found.  Falling back to base PHP template.
 				$template['type'] = 'php';
@@ -26,11 +28,11 @@ function find_template($section)
 			if ($haml_exists) {
 				// Haml template exists -- use it.
 				$template['type'] = 'haml';
-				$template['name'] = $section;
+				$template['name'] = $page['section'];
 			} elseif ($php_exists) {
 				// Wanted Haml, but it's not there and the PHP is -- use that instead.
 				$template['type'] = 'php';
-				$template['name'] = $section;
+				$template['name'] = $page['section'];
 			} else {
 				// No template found.  Falling back to base Haml template.
 				$template['type'] = 'haml';
@@ -44,6 +46,8 @@ function find_template($section)
 
 function find_page($section, $subsection, $page_name)
 {
+	$section_path = $subsection_path = '';
+
 	// If there is a section or sub-section, add a trailing slash.
 	if (!empty($section))    $section_path    = $section 	. '/';
 	if (!empty($subsection)) $subsection_path = $subsection . '/';
@@ -52,13 +56,13 @@ function find_page($section, $subsection, $page_name)
 
 	// Check to see if the page exists
 	if (file_exists(CONTENT_PATH . '/' . $page_path . '.txt')) {
-		$page['section'] = $section;
-		$page['subsection'] = $subsection;
 		$page['path'] = $page_path;
 	} else {
-		$page['section'] = 'error';
+		// $page['section'] = 'error';
 		$page['path'] = 'errors/404';
 	}
+	$page['section'] = $section;
+	$page['subsection'] = $subsection;
 	$page['name'] = $page_name;
 	
 	return $page;
@@ -79,7 +83,7 @@ function display_page($template, $page)
 				<tr><th>This Template:</th><td>${template['type']}</td></tr>
 				<tr><th>Section:</th><td>${page['section']}</td></tr>
 				<tr><th>Subsection:</th><td>${page['subsection']}</td></tr>
-				<tr><th>Page:</th><td>${page['name']}</td></tr>
+				<tr><th>Page Name:</th><td>${page['name']}</td></tr>
 			</table>
 EOT;
 	}
@@ -89,7 +93,7 @@ EOT;
 
 	// Generate the page with PHP or Haml
 	switch ($template['type']) {
-		// TODO: Send proper 404 header if $page['section'] = 'error';
+		// TODO: Send proper 404 header if $page['path'] = 'errors/404';
 		case 'php':
 			include(TEMPLATE_PATH . '/' . $template['name'] . '.php');
 			break;
--- public/index.php
+++ public/index.php
@@ -2,5 +2,8 @@
 require_once('../app/load.php');
 require_once('../app/functions.php');
 
-display_page(find_template($section), find_page($section, $subsection, $page_name));
+$page = find_page($section, $subsection, $page_name);
+$template = find_template($page);
+
+display_page($template, $page);
 ?>
