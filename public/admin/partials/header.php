ec4d86e15cf39f0676d3004d4f2c64925e96aad4
Trey Piepmeier <trey@treypiepmeier.com>
2007-12-23 23:30:24 -0600

    Adding files I didn't checkin last time--admin stuff.

---
+++ public/admin/partials/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+	<title>Yark: Login</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+	<link rel="stylesheet" href="css/main.css" type="text/css" media="all">
+	<!--[if lte IE 7]><link rel="stylesheet" href="css/iewin.css" type="text/css" media="screen"><![endif]-->
+	<!--[if lte IE 6]><link rel="stylesheet" href="css/ie6win.css" type="text/css" media="screen"><![endif]-->
+	<!--[if lte IE 5]><link rel="stylesheet" href="css/ie5win.css" type="text/css" media="screen"><![endif]-->
+	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/yark.js" type="text/javascript"></script>
+</head>
+<body>
+	<form action="admin" method="post">
