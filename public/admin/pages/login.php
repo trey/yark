83fc5f645ceddeeb2bad3a8323b70aad90526777
Trey Piepmeier <trey@treypiepmeier.com>
2007-12-27 23:47:27 -0600

    whoops again

---
+++ public/admin/pages/login.php
@@ -0,0 +1,10 @@
+<?php require_once('partials/header.php'); ?>
+
+<div id="login">
+	<div id="header">
+		<h1><img src="img/yark.gif" width="274" height="190" alt="Yark" /></h1>
+	</div><!-- #header -->
+	<div><input type="password" name="password" value="" id="password" /></div>
+</div><!-- #login -->
+
+<?php require_once('partials/footer.php'); ?>
